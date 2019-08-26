4bfcce0931b19421dd874a241abe01da6d7efd21
Marianne Linardos <mlinardos.edu@gmail.com>
2019-08-26 12:38:09 -0400

    Add project files.

---
+++ WebAPI_Demo/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Demo.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+
+    }
+}
---
+++ WebAPI_Demo/Models/WebServiceDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Demo.Models            // not a poco - inherits from DBContext
+{
+    public class WebServiceDB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }          // Dependancy Injection requires constructor
+
+        public WebServiceDB(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
